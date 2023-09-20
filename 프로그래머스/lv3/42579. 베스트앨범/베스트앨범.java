12136f828bbbd2192edd42e19144a6ae8967ec93
kimjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-09-20 13:52:28 +0900

    [level 3] Title: 베스트앨범, Time: 4.63 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
+        
         HashMap<String, Integer> songs = new HashMap<>();
         for(int i = 0; i < genres.length; i++){
             songs.put(genres[i], songs.getOrDefault(genres[i], 0) + plays[i]);
         }
         
         List<Map.Entry<String, Integer>> entryList = 
-            new LinkedList<>(songs.entrySet());
+            new ArrayList<>(songs.entrySet());
         
         entryList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        
         List<Integer> list = new ArrayList<>();
         for(Map.Entry<String, Integer> entry : entryList){
-            System.out.println(entry.getKey());
             int arr[] = {-1, -1};
             int firstMaxPlay = 0;
             int secondMaxPlay = 0;
