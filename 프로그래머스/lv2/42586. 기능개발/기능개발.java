9723dbc29df02f565eac778de66e18221703cd48
kimjihun <62210749+K-IMjihun@users.noreply.github.com>
2023-09-21 16:25:50 +0900

    [level 2] Title: 기능개발, Time: 0.32 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i = 0; i < progresses.length; i++){
+            int progressesCount = progresses[i];
+            int count = 0;
+            while(progressesCount < 100){
+                progressesCount += speeds[i];
+                count++;
+            }
+            q.add(count);
+            System.out.println(count);
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        int count = 1;
+        int a = q.poll();
+        
+        while(0 < q.size()){
+            if(a >= q.peek()){
+                q.poll();
+                count++;
+                if(q.isEmpty()){
+                    list.add(count);
+                }
+            }
+            
+            else{
+                list.add(count);
+                count = 1;
+                a = q.poll();
+                System.out.println(q.size());
+                            if(q.isEmpty()){
+                list.add(count);
+            }
+                continue;
+            }
+
+
+        }
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
