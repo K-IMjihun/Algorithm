185f4b5f4ccad782a808f33afc82c6315af69ee7
kimjihun <62210749+K-IMjihun@users.noreply.github.com>
2024-04-01 15:08:59 +0900

    [level 2] Title: 구명보트, Time: 8.21 ms, Memory: 53.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+    int answer = 0;
+    Arrays.sort(people);
+    int count = 0;
+    for(int i = people.length-1; i >= count; i--){
+      if(people[i] + people[count] <= limit){
+        answer++;
+        count++;
+      }
+      else answer++;
+    }
+    return answer;
+  }
+}
