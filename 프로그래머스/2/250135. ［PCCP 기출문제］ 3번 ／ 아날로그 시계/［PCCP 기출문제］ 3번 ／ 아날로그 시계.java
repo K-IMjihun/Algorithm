8170db522595432e58070d214c55f968ca61ee86
kimjihun <62210749+K-IMjihun@users.noreply.github.com>
2024-05-03 15:46:36 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 0.04 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int h1, int m1, int s1, int h2, int m2, int s2) {
+        int answer = -1;
+        
+        int start = toSecond(h1,m1,s1);
+        int end = toSecond(h2,m2,s2);
+        
+        answer = cal(end) - cal(start) + (alramNow(start)? 1:0); 
+        
+        return answer;
+    }
+    static int cal(int time){
+        int sm = time*59/3600; 
+        int sh = time*719/43200;
+        int a = 43200 <= time ? 2 : 1;// 43200(12시간) 이상이면 2, 이하면 1 
+
+        return sm+sh - a;
+    }
+    
+    static int toSecond(int h,int m,int s){
+        int k = h*3600+m*60+s;
+        return k;
+    }
+    static boolean alramNow(int time){
+        return time*59%3600==0 || time*719%43200==0; // 현재시간에 알람이 울리는지 체크
+    }
+}
