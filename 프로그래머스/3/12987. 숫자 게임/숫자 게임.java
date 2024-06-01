6bafb6318274012329430b1c5f22a9945ab617e7
kimjihun <62210749+K-IMjihun@users.noreply.github.com>
2024-06-01 21:50:11 +0900

    [level 3] Title: 숫자 게임, Time: 54.20 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int aIndex = 0;
+        int bIndex = 0;
+        int answer = 0;
+
+        while (bIndex < B.length) {
+            if (B[bIndex] > A[aIndex]) {
+                answer++;
+                aIndex++;
+            }
+            bIndex++;
+        }
+
+        return answer;
+    }
+}
