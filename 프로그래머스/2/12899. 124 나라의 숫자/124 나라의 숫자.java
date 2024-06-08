6c37ee3ed98c58501bbf1669cd3e0ace8fdf19c9
kimjihun <62210749+K-IMjihun@users.noreply.github.com>
2024-06-08 19:25:38 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.07 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+
+        while (n > 0) {
+            int remainder = n % 3;
+            n /= 3;
+
+            if (remainder == 0) {
+                remainder = 4;
+                n--;
+            }
+
+            answer.insert(0, remainder);
+        }
+
+        return answer.toString();
+    }
+}
